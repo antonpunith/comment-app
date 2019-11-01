d833a22beb47e0151f4492ac9153b06b68489802
Anton Punith <antonpunith@gmail.com>
2019-11-01 15:39:53 +1100

    adding comment section

---
+++ src/components/CommentSection/index.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+// styles
+import './styles.css';
+
+class CommentSection extends Component {
+  render () {
+    return (
+      <section className="comments">
+        <h1>Comments</h1>
+      </section>
+    );
+  }
+}
+
+export default CommentSection;
